6c0e3af4322e4799708847e0682dc202b60540b5
EvecySon <michaelok290@gmail.com>
2026-03-07 13:29:21 +0000

    Add complete ABC Transport and GIG Logistics pickup locations across Nigeria

--- src/components/pages/CheckoutPage.tsx
+++ src/components/pages/CheckoutPage.tsx
@@ -163,14 +163,242 @@ const PICKUP_LOCATIONS: { [key: string]: { [state: string]: string[] } } = {
     ]
   },
   'GIG': {
-    'Lagos': ['Ikeja Hub', 'Yaba Office', 'Victoria Island'],
-    'Abuja': ['Wuse Terminal', 'Gwarinpa Hub'],
-    'default': ['Main Hub']
+    'Lagos': [
+      'AJAH 1 - KM 25, Lekki-Epe Expressway, Ajiwe-Ajah',
+      'AJAH 2 - KM 22, Lekki-Epe Expressway, by Abraham Adesanya Roundabout',
+      'AKOWONJO - 41 Shasha Road, Akowonjo Junction, Dopemu',
+      'ALABA - CS1 Ground Floor, CornerStone Plaza, Alaba International Market',
+      'BROAD STREET - 158 Broad Street, Lagos Island (Behind UBA Head Office)',
+      'CELE-OKOTA - 2 Okota Road, Beside Zenith Bank / 103 Okota Road',
+      'ELEGANZA - Block B, Eleganza Shopping Mall, Lekki-Epe Express Road',
+      'EPE - Animashaun Plaza, Beside Petrocam Filling Station',
+      'FADEYI - 69 Ikorodu Road, Fadeyi',
+      'FESTAC - 1st Avenue Road, Festac First Gate, Beside INEC Office',
+      'GBAGADA - 7 Hospital Road, Ifako, Gbagada',
+      'GBAGADA EXPRESS - No. 1 Sunday Ogunyade Street, Beside Eterna Filling Station',
+      'IGANDO - Eunice Ato Filling Station, College Bus Stop',
+      'IGBO EFON - KM 17, Scapular Plaza',
+      'IKEJA - 9 Medical Road, Opposite Zenith Bank',
+      'IKOYI - 103 Awolowo Road',
+      'IKOTUN - 29 Idimu Road, Opp. Local Govt. Council',
+      'ILUPEJU - 13C, Ilupeju Bypass',
+      'TRADE FAIR - Shop D77 & D78, Abia Plaza, BBA',
+      'IPAJA - 164 Lagos Abeokuta Express Way, Beside Diamond Bank',
+      'JIBOWU - 20 Ikorodu Express Road',
+      'LEKKI ADMIRALTY - No 1A, Wole Ariyo Street, Beside First Bank, Phase 1',
+      'LEKKI 2 - Scapula Plaza, Lekki-Epe Express Road, Peninsula II',
+      'OLD OJO ROAD - By Police Station',
+      'OGBA - 3 Ijaiye Road, Beside FCMB',
+      'OPEBI - 62 Opebi Road, Ikeja',
+      'OYINGBO - No 25, Otto Causeway Opp Iddo Bus Stop',
+      'SANGO - 3 Abeokuta-Lagos Expressway, Opp. Sango Bridge',
+      'SURULERE - 78 Ogunlana Drive',
+      'VICTORIA ISLAND - 1436 Sanusi Fafunwa Street',
+      'VOLKS - 169 Badagry Expressway, Volkswagen Bus Stop',
+      'YABA - Tejuosho Ultra Modern Market, Ojuelegba Road'
+    ],
+    'FCT': [
+      'GARKI - Shop C11, Efab Plaza, Area 11',
+      'GWARIMPA 1 - House 38, 3rd Avenue, Opposite Union Bank',
+      'GWARIMPA 2 - Suite A1, Bricks and More Plaza, 4th Avenue',
+      'GWAGWALADA - Ajibada Plaza, Shop 10, Plot 48, Along Park Road',
+      'KUBWA - Block 43, Gado Nasko Way, Opp. 2/2 Court',
+      'MARABA - 132, Giga Plaza, via Nyanya, Opp. Chrisgold Plaza',
+      'MADALLA - Near Mobil Filling Station, Along Kaduna Express Road',
+      'UTAKO 1 - Plot 113, I.V.W Osisiogu Str, Beside Utako Police Station',
+      'UTAKO 2 - Suite A13, Abraham Plaza, Plot 6 Ekikunam Street',
+      'WUSE - Plot 80 Aminu Kano Crescent, Opp. Shariff Plaza',
+      'ZUBA - 206, Zuba Market, Opp. Lagos Line'
+    ],
+    'Rivers': [
+      'PHC ADA GEORGE - 18 Ada George, By Okilton Junction',
+      'PHC ALAKAHIA - Linus Book Shop Building, East-West Road',
+      'PHC ARTILLERY - Cocaine Village Junction, Off Aba Road, Rumuogba',
+      'PHC ELELENWO - 299 Old Refinery Road, By De-Young Junction',
+      'PHC ELIOZU - Emmanuel Plaza, G.U. Ake Road, Beside Planet Filling Station',
+      'PHC OLU OBASANJO - 61 Olu Obasanjo Road, Opposite Roundabout',
+      'PHC PETER ODILI - 89 Peter Odili Road, Besides Eterna Filling Station',
+      'PHC STADIUM ROAD - 9 Stadium Road, Beside Benjack',
+      'PHC TOMBIA - 67 Tombia Ext, GRA',
+      'PHC WOJI - Agora Plaza, 118 Woji Road, By Bodo Junction, GRA Phase 2',
+      'PHC CHOBA - LACFOG PLAZA Adjacent Choba Police Station, East-west Road'
+    ],
+    'Oyo': [
+      'IBADAN 1 - Town Planning Complex, By Sumal Foods, Ring Road',
+      'IBADAN 2 - Suite 5, Kamal Memorial Plaze, Bodija-UI Road',
+      'IBADAN 3 - Bovas Filling Station, 106/107 Agbaakin Layout, Iwo Road',
+      'IBADAN 4 - Block E2, Preboye Shopping Complex Sango Ojoo',
+      'OGBOMOSHO - Eterna Filling Station, Opposite Zenith Bank, Starlight'
+    ],
+    'Abia': [
+      'ABA 1 - No 5, Asa Road, Former/Old Nitel Building',
+      'ABA 2 - 30 Brass Street, G.R.A, After Jevinic Restaurant',
+      'UMUAHIA 1 - 8 Mission Hill, Opposite Villaroy Hotel',
+      'UMUAHIA 2 - 60 Aba Road, Close to MTN Office',
+      'UMUAHIA 3 - Aba-Port-Harcourt Expressway, Ohiya'
+    ],
+    'Anambra': [
+      'AWKA - Elite Shopping Complex, Enugu/Onitsha Expressway',
+      'ONITSHA - 2 Awka Road, By DMGS Junction, Beside All Saints Cathedral',
+      'NNEWI - No 73 Owerri Road, Martina Chukwuma Plaza (Innoson Plaza)'
+    ],
+    'Bauchi': [
+      'BAUCHI - Shop 7, Yandoka Road, Adjacent MTN office'
+    ],
+    'Bayelsa': [
+      'YENAGOA - Kpansia Epia, Opposite Wema Bank by INEC Junction'
+    ],
+    'Benue': [
+      'MAKURDI - No 4, Old Otukpo Road, Opposite Dester\'s'
+    ],
+    'Cross River': [
+      'CALABAR - 29 Ndidem Usang Iso Road (Aka Marian Road)'
+    ],
+    'Delta': [
+      'ASABA 1 - Asaba Onitsha Expressway, By Head Bridge',
+      'ASABA 2 - 445, Nnebisi Road, Opposite Zenith Bank',
+      'WARRI 1 - 138, Effurun-Sapele Rd, By Airport Junction',
+      'WARRI 2 - 116, Effurun-Sapele Warri Road, Effurun'
+    ],
+    'Ebonyi': [
+      'ABAKALIKI - 1A, Ogoja Road (Beside Ecobank)'
+    ],
+    'Edo': [
+      'BENIN AKPAKPAVA - 112, Akpakpava Road',
+      'BENIN AIRPORT - Shop 1, Omegatron Plaza, 47 Airport Road',
+      'BENIN RAMAT PARK - 42, Benin/Agbor Road, Oregbeni',
+      'BENIN USELU - 202, Uselu Lagos Road, Ugbowo',
+      'AUCHI - Okene Express Way, Opp Auchi Polytechnic',
+      'EKPOMA - Ukpenu Road Opp. Grail Message Centre, Akahia'
+    ],
+    'Ekiti': [
+      'ADO EKITI - Sola Dola Filling Station, Along Ikere Road, Ajilosun'
+    ],
+    'Enugu': [
+      'ENUGU 1 - 7, Market Road, Holy Ghost Park, Opp. State Library',
+      'ENUGU 2 - 67, Zik Avenue Uwani'
+    ],
+    'Imo': [
+      'OWERRI - Plot C31, Relief Road, by Relief Junction, Off Egbu Road'
+    ],
+    'Kaduna': [
+      'KADUNA 1 - 8 Ahmadu Bello Way, Off Kastina Roundabout',
+      'KADUNA 2 - Lagos Garage by Airforce Mami Mando',
+      'KADUNA 3 - Nnamdi Azikiwe Expressway, By Command Junction',
+      'ZARIA - Dairu Garba Plaza, 18 Sokoto Road, MTD Junction'
+    ],
+    'Kano': [
+      'KANO - 69 Murtala Mohammed Way / No 1 Bompai Road'
+    ],
+    'Kwara': [
+      'ILORIN 1 - 190, Ibrahim Taiwo Road, Adjacent Chicken Republic/UBA',
+      'ILORIN 2 - No 1 Umar Audi road, Fate Road, Tanke GRA'
+    ],
+    'Nasarawa': [
+      'LAFIA - Shops 1 & 2 Police Officers Mess, Opposite Polaris Bank'
+    ],
+    'Niger': [
+      'MINNA - Shop J4, Awalu Ibrahim Shopping Complex, Tunga'
+    ],
+    'Ogun': [
+      'ABEOKUTA 1 - 62, Tinubu Street, Ita Eko',
+      'ABEOKUTA 2 - Alumni Building, Federal University of Agric (FUNAAB)',
+      'IJEBU ODE - 102 Ibadan Road, Opposite NEPA Office',
+      'SANGO OTTA - 3 Abeokuta-Lagos Expressway, Beside 9mobile Office'
+    ],
+    'Ondo': [
+      'AKURE - No 22 Oyemekun Road by Cathedral Junction'
+    ],
+    'Osun': [
+      'OSOGBO - Ogo Oluwa Bus Stop, Gbangan/Ibadan Road',
+      'GBONGAN - Gbongan Branch'
+    ],
+    'Plateau': [
+      'JOS 1 - Suite B3, Emerald Royal Plaza, Zaramangada',
+      'JOS 2 - Angwan Soya Zaria Road Bypass'
+    ],
+    'Sokoto': [
+      'SOKOTO - 3/4 Maiduguri Road, Gawon Nama Area'
+    ],
+    'Taraba': [
+      'JALINGO - 106 White Castle Plaza, Barde Way'
+    ],
+    'Yobe': [
+      'DAMATURU - Zainami Plaza, Shop Z15, Gujba Roundabout'
+    ],
+    'default': ['Contact Customer Service for Nearest Location']
   },
   'ABC': {
-    'Lagos': ['Ojota Terminal', 'Oshodi Park'],
-    'Abuja': ['Jabi Terminal'],
-    'default': ['Main Park']
+    'Abia': [
+      'ABA - 2, Ikot Ekpene Road',
+      'UMUAHIA - 8 Mission Hill'
+    ],
+    'Anambra': [
+      'ONITSHA - Onitsha-Asaba Expressway, Opp Ogbaru Main Market'
+    ],
+    'Akwa Ibom': [
+      'UYO - Long Distance Bus Terminal, Park Road, Itam'
+    ],
+    'Bayelsa': [
+      'YENAGOA - No 47, Erepa Road, Opposite The Rainbow Hospital'
+    ],
+    'Cross River': [
+      'CALABAR - Calabar Terminal'
+    ],
+    'Delta': [
+      'WARRI - 194 PTI Road, Effurun'
+    ],
+    'Edo': [
+      'BENIN - By Terminal Resort & Park, Ovbiogie'
+    ],
+    'Enugu': [
+      'ENUGU - 122 Ogui Road, Beside Union Bank Plc',
+      'NGWO - 9th Mile Corner, Ngwo'
+    ],
+    'FCT': [
+      'UTAKO - 36 Ekukinam Street, Utako District (off Berger Junction)',
+      'GWAGWALADA - Jibeco Nigeria Ltd, Gwagwalada'
+    ],
+    'Imo': [
+      'OWERRI - Km 5, MCC Uratta Road, Umuoba Uratta',
+      'MBAISE - Mbaise Terminal'
+    ],
+    'Kogi': [
+      'LOKOJA - Lokoja Terminal'
+    ],
+    'Ogun': [
+      'IJEBU ODE - Ijebu Ode Branch'
+    ],
+    'Plateau': [
+      'JOS - Jos Terminal'
+    ],
+    'Rivers': [
+      'PORT HARCOURT - Eliozu Junction, East/West Road'
+    ],
+    'Lagos': [
+      'AMUWO-ODOFIN - Plot 79, Oba Kayode Akinyemi Way, by Festac Bypass',
+      'BOLADE OSHODI - 129 Agege Motor Road, Oshodi',
+      'JIBOWU - 22 Ikorodu Road, Yaba',
+      'IKEJA CARGO - 2 Simbiat Abiola Way, Computer Village',
+      'LEKKI/JAKANDE - Opposite Jakande 1st Gate, Lekki Expressway',
+      'AJAH - Km 51 Lekki-Epe Expressway, opposite Enyo fueling station',
+      'OKOTA - Block H, Atinuke Shopping Complex, 129 Okota Rd',
+      'MAZAMAZA - No 35 Old Ojo Road, by Dandolas Park',
+      'FADEYI - 52C Ikorodu Road, Fadeyi (Head Office)',
+      'ALABA - Alaba International Market area',
+      'AKOWONJO - Akowonjo area',
+      'FESTAC - Festac Town',
+      'IKORODU - Ikorodu area',
+      'TEJUOSHO - Tejuosho Market area',
+      'BALOGUN - Balogun area',
+      'OGBA - Ogba area',
+      'AWOYAYA - Awoyaya area',
+      'IKOTUN - Ikotun area',
+      'IDIMU - Idimu area',
+      'LEKKI PHASE 1 - Lekki Phase 1',
+      'IYANA IPAJA - Iyana Ipaja area'
+    ],
+    'default': ['Contact Customer Service for Nearest Location']
   },
   'PARK': {
     'Lagos': ['Ojuelegba Terminal', 'Surulere Hub'],
